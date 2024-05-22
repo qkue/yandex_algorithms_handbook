fa70832d8c7d5034e7fdb623288e23b65c6ff868
qkue <130838079+qkue@users.noreply.github.com>
2024-05-22 21:39:18 +0300

    Update solution.py

--- 3.5 Алгоритмы «Разделяй и властвуй»/A/solution.py
+++ 3.5 Алгоритмы «Разделяй и властвуй»/A/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+def select_sort(seq):
+    for i in range(len(seq) - 1):
+        min_index = i
+        for k in range(i + 1, len(seq)):
+            if seq[k] < seq[min_index]:
+                min_index = k
+        seq[i], seq[min_index] = seq[min_index], seq[i]
+    return seq
+
+n = int(input())
+a = list(map(int, input().split()))
+
+print(*select_sort(a))
