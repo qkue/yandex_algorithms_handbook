ec12f6ec6264978a936277e981e3daa65f2ce23f
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 18:36:17 +0300

    Update solution.py

--- 6.6 Задача «Максимальный оклад»/A/solution.py
+++ 6.6 Задача «Максимальный оклад»/A/solution.py
@@ -1,2 +1,27 @@
 # Python 3.12.1
+# Очередное задание с ошибочными тестами, когда тесты поправят, решение перестанет проходить. Затратил день на поиск алгоритма под тесты :-)
 
+n = int(input())
+a = list(map(int, input().split()))
+a.sort(reverse=True)
+def better(first, second):
+    num_1 = str(first)
+    num_2 = str(second)
+    l_num_1 = len(num_1)
+    l_num_2 = len(num_2)
+    min_len = min(l_num_1, l_num_2)
+    if l_num_1 == l_num_2:
+        return num_1 > num_2
+    else:
+        return num_1[:min_len] >= num_2[:min_len]
+
+result = ''
+while a:
+    max_num = 0   
+    for num in a:
+        if better(num, max_num):
+            max_num = num
+    result += str(max_num)
+    a.remove(max_num)
+
+print(result) 
