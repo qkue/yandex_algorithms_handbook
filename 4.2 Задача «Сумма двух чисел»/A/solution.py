dbc9702c745afff914340d1f4c559961c243a541
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 11:14:05 +0300

    Update solution.py

--- 4.2 Задача «Сумма двух чисел»/A/solution.py
+++ 4.2 Задача «Сумма двух чисел»/A/solution.py
@@ -1,2 +1,8 @@
 # Python 3.12.1
 
+def sum_of_digits(first_digit, second_digit):
+    return first_digit + second_digit
+ 
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(sum_of_digits(a, b))
