96c6f05a3607839e06471d2d164e7dd9fb608d6e
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 15:22:47 +0300

    Update solution.py

--- 5.2 Вычисление НОК и НОД/B/solution.py
+++ 5.2 Вычисление НОК и НОД/B/solution.py
@@ -1,2 +1,13 @@
 # Python 3.12.1
 
+def GCD(a, b):
+    while a > 0 and b > 0:
+        if a >= b:
+            a = a % b
+        else:
+            b = b % a
+    return max(a,b)
+
+
+n, m = map(int, input().split())
+print((n * m) // GCD(n, m))
