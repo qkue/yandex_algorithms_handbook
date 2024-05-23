22dd581b24e8492a7e5060718cca04553325ad02
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 14:21:38 +0300

    Update solution.py

--- 4.3 Задача «Максимальное произведение»/C/solution.py
+++ 4.3 Задача «Максимальное произведение»/C/solution.py
@@ -1,2 +1,11 @@
 # Python 3.12.1
 
+def max_pair(arr):
+    arr.sort()
+    right = arr[-2] * arr[-1] * arr[-3]
+    left = arr[0] * arr[1] * arr[-1]
+    print(max(right, left))
+
+n = int(input())
+m = list(map(int, input().split()))
+max_pair(m)
