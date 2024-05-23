a6637ea42d1797ae73669e1292ff178bb79e1fd7
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 18:18:20 +0300

    Update solution.py

--- 6.4 Задача «Сбор подписей»/A/solution.py
+++ 6.4 Задача «Сбор подписей»/A/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n = int(input())
+lr = sorted([tuple(map(int, input().split())) for _ in range(n)], key = lambda x: x[1])
+
+count = 0
+points = []
+
+for point in lr:
+    left, right = point
+    if not points or left > points[-1]:
+        points.append(right)
+        count += 1
+print(count)
+print(*points)
