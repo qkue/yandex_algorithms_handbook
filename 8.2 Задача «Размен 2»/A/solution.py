e6b07027566e83f4488a40cfdc243c63bbb88143
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 22:11:05 +0300

    Update solution.py

--- 8.2 Задача «Размен 2»/A/solution.py
+++ 8.2 Задача «Размен 2»/A/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+def change(money):
+    table = [10**3 for i in range(money+1)]
+    table[0] = 0
+
+    for m in range(1, money+1):
+        for c in [1, 3, 4]:
+            if c <= m:
+                table[m] = min(table[m], 1 + table[m-c])
+    return table[money]
+
+
+m = int(input())
+print(change(m))
+
