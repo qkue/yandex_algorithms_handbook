eab04b6028e7007916e262dac52e3f2dfe997313
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 17:15:04 +0300

    Update solution.py

--- 6.2 Задача «Специи»/B/solution.py
+++ 6.2 Задача «Специи»/B/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+
+n, s = map(int, input().split())
+
+things = [int(input()) for _ in range(n)]
+things.sort(reverse=True)
+count = 0
+
+while things:
+    if things[-1] <= s:
+        s -= things.pop()
+        count += 1
+    else:
+        things.pop()
+print(count)
