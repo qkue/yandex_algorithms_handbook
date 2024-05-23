3e03329c05cbe4aed2af564a78d256751af963fb
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 17:32:19 +0300

    Update solution.py

--- 6.3 Задача «Рекламная компания»/A/solution.py
+++ 6.3 Задача «Рекламная компания»/A/solution.py
@@ -1,2 +1,10 @@
 # Python 3.12.1
 
+n = int(input())
+p = sorted(list(map(int, input().split())))
+c = sorted(list(map(int, input().split())))
+result = 0
+
+for i in range(n):
+    result += p[i] * c[i]
+print(result)
