26e604004373595489909ddf004e7bc76fd50cd3
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 15:53:51 +0300

    Update solution.py

--- 6.1 Задача «Размен»/B/solution.py
+++ 6.1 Задача «Размен»/B/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
 
+n = int(input())
+nominal = [1, 5, 10, 20, 50]
+
+numCoins = 0
+final = []
+
+for i in nominal[::-1]:
+    if n >= i:
+        numCoins += n // i
+        final += [i] * (n // i)
+        n = n % i
+
+print(numCoins)
+print(*final)
