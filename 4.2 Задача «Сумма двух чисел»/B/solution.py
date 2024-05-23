42c97f748033aa548bc8b9a469d59f6b3b2547fd
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 11:26:34 +0300

    Update solution.py

--- 4.2 Задача «Сумма двух чисел»/B/solution.py
+++ 4.2 Задача «Сумма двух чисел»/B/solution.py
@@ -1,2 +1,18 @@
 # Python 3.12.1
 
+def many(a_x, b_x, arr_a, arr_b):
+    print(max(a_x, b_x))
+    ind = -1
+
+    for i in arr_b[::-1]:
+        arr_a[ind] += i
+        ind -= 1
+    print(*arr_a)        
+
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+if len(a) != len(b):
+    a, b = max(a, b, key=len), min(a, b, key=len)
+many(n, m, a, b)
