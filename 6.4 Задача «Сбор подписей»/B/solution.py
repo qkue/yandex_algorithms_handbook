c0886bb8423140720e62810e209979a00a238f60
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 18:21:24 +0300

    Update solution.py

--- 6.4 Задача «Сбор подписей»/B/solution.py
+++ 6.4 Задача «Сбор подписей»/B/solution.py
@@ -1,2 +1,13 @@
 # Python 3.12.1
 
+n, el = map(int, input().split())
+xi = sorted(list(map(int, input().split())))
+
+count = 0
+r_point = None
+
+for x in xi:
+    if r_point is None or x > r_point:
+        count += 1
+        r_point = x + el
+print(count)
