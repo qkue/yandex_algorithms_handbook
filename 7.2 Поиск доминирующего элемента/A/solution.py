b193f257e1e315de92332c3eae38716cec40101b
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 20:36:11 +0300

    Update solution.py

--- 7.2 Поиск доминирующего элемента/A/solution.py
+++ 7.2 Поиск доминирующего элемента/A/solution.py
@@ -1,2 +1,20 @@
 # Python 3.12.1
 
+n = int(input())
+ax = list(map(int, input().split()))
+#ax.sort()
+
+def major(a):
+    count = 0
+    candidate = 0
+    for person in a:
+        if count == 0:
+            candidate = person
+            count += 1
+        else:
+            if person == candidate:
+                count += 1
+            else:
+                count -= 1
+    return (1 if count else 0)
+print(major(ax))
