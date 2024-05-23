238f25efd527dc1c87f0265ff049692de73b66df
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 17:11:01 +0300

    Update solution.py

--- 6.2 Задача «Специи»/A/solution.py
+++ 6.2 Задача «Специи»/A/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+n, w = map(int, input().split())
+
+spicy = [tuple(map(float, input().split())) for _ in range(n)]
+spicy.sort(key = lambda x: x[0] / x[1])
+cost = 0
+
+while w > 0 and spicy:
+    loop_spicy_c, loop_spicy_w = spicy.pop()
+    loop_amount = min(loop_spicy_w, w)
+    value = (loop_spicy_c / loop_spicy_w) * loop_amount
+    w -= loop_amount
+    cost += value
+print(cost)
