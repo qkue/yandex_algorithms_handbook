0b143c3620d6cec4725e895e3fe4948213ad6c4d
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 11:44:23 +0300

    Update solution.py

--- 4.2 Задача «Сумма двух чисел»/D/solution.py
+++ 4.2 Задача «Сумма двух чисел»/D/solution.py
@@ -1,2 +1,11 @@
 # Python 3.12.1
 
+n, m = map(int, input().split())
+matrix_f = [list(map(int, input().split())) for k in range(n)]
+matrix_s = [list(map(int, input().split())) for k in range(n)]
+
+res = [[matrix_f[j][i] + matrix_s[j][i] for i in range(m)] for j in range(n)]
+
+for x in range(n):
+    for y in range(m):
+        print(res[x][y], end=' ')
