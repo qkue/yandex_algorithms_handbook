92f1689df8cce89993b18a0919d259b87a2edcbe
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 23:08:30 +0300

    Update solution.py

--- 9.3 Словарь/A/solution.py
+++ 9.3 Словарь/A/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+unknown_dict = dict()
+answer = []
+
+q = int(input())
+for _ in range(q):
+    query = input().split()
+    if query[0] == '1':
+        unknown_dict[query[1]] = query[2]
+    elif query[0] == '2':
+        temp = unknown_dict.get(query[1], '-1')
+        answer.append(temp)
+
+print('\n'.join(answer))
