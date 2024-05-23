c7363bbfa4994e9e49c56e87ff829f3f90f24d88
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 18:10:48 +0300

    Update solution.py

--- 6.3 Задача «Рекламная компания»/B/solution.py
+++ 6.3 Задача «Рекламная компания»/B/solution.py
@@ -1,2 +1,16 @@
 # Python 3.12.1
+# Решение до банального простое, гораздо больше времени ушло на понимание условия задачи. Пример задачи с подвохом.
 
+n, k, w = map(int, input().split())
+order = sorted([list(map(int, input().split())) for _ in range(k)], key = lambda x: -x[0])
+
+result = 0
+
+place = n * w
+for i in order:
+    if place:
+        c, p = i
+        amount = min(p, place)
+        result += c * amount
+        place -= amount
+print(result)
