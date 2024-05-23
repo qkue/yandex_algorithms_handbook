3102d42b2fed874403dff4e2f65eccc5035f5e69
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 18:55:08 +0300

    Update solution.py

--- 6.6 Задача «Максимальный оклад»/C/solution.py
+++ 6.6 Задача «Максимальный оклад»/C/solution.py
@@ -1,2 +1,19 @@
 # Python 3.12.1
 
+r, c = map(int, input().split())
+
+kings = 0
+if r * c < 2:
+    kings = 0
+elif r == 1 or c == 1:
+    temp = max(r, c)
+    if temp % 3 != 0:
+        kings += temp - (temp // 3 + 1)
+    else:
+        kings += temp - (temp // 3) 
+else:
+    r_full = r - (r // 3 + [0, 1][r % 3 != 0])
+    r_empty = r - r_full
+    fill_row = c - (c // 3 + [0, 1][c % 3 != 0])
+    kings = r_full * c + r_empty * fill_row
+print(kings)
