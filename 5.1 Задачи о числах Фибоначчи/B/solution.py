156269203c7db153c4dfab3dd33a3d9b14832441
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 14:59:24 +0300

    Update solution.py

--- 5.1 Задачи о числах Фибоначчи/B/solution.py
+++ 5.1 Задачи о числах Фибоначчи/B/solution.py
@@ -1,2 +1,13 @@
 # Python 3.12.1
 
+def fib(a):
+    if a <= 1:
+        return a
+    f_0, f_1 = 0, 1
+    for i in range(1, a):
+        f_0, f_1 = f_1, (f_0 + f_1) % 10
+    return f_1
+
+
+n = int(input())
+print(fib(n))
