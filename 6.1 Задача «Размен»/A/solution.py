d7deeed1da185c6f3a6cb398adc13fd80469bb57
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 15:38:12 +0300

    Update solution.py

--- 6.1 Задача «Размен»/A/solution.py
+++ 6.1 Задача «Размен»/A/solution.py
@@ -1,2 +1,15 @@
 # Python 3.12.1
 
+final = []
+cnt = 0
+n = int(input())
+
+for ten in range((n // 10) + 1):
+        
+    for five in range(((n - (10 * ten)) // 5) + 1):
+        one = n - 10 * ten - 5 * five
+        steps = str(ten + five + one)        
+        final.append((steps + ' ' + '10 ' * ten + '5 ' * five + '1 ' * one).rstrip())
+        cnt += 1
+print(cnt)
+print(*final, sep='\n')
