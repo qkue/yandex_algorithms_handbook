edf2933c1151ebb18cb0e43362d093abbb74367c
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 22:15:10 +0300

    Update solution.py

--- 9.2 Множество/A/solution.py
+++ 9.2 Множество/A/solution.py
@@ -1,2 +1,18 @@
 # Python 3.12.1
 
+numbers = set()
+
+n = int(input())
+answer = []
+
+for _ in range(n):
+    q, num = input().split()
+    if q == '1':
+        numbers.add(num)
+    elif q == '2':
+        if num in numbers:
+            answer.append('1')
+        else:
+            answer.append('0')
+            
+print('\n'.join(answer))
