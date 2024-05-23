1eec7069f4c327ff87d70f3388ea2862c173cc64
qkue <130838079+qkue@users.noreply.github.com>
2024-05-23 11:51:51 +0300

    Update solution.py

--- 4.3 Задача «Максимальное произведение»/A/solution.py
+++ 4.3 Задача «Максимальное произведение»/A/solution.py
@@ -1,2 +1,10 @@
 # Python 3.12.1
 
+def MaxPairwiseProductBySorting(a):
+    a.sort()
+    return a[-1]*a[-2]
+
+n = int(input())
+m = list(map(int, input().split()))
+
+print(MaxPairwiseProductBySorting(m))
