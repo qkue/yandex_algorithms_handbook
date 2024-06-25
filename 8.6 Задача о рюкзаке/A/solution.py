5a05f53d7c1cb4af8376f82924a2a08a301c86ec
qkue <130838079+qkue@users.noreply.github.com>
2024-06-25 16:37:08 +0300

    Update solution.py

--- 8.6 Задача о рюкзаке/A/solution.py
+++ 8.6 Задача о рюкзаке/A/solution.py
@@ -1,2 +1,27 @@
-# Python 3.12.1
+# Python 3.12.3
+
+from sys import stdin
+
+w, n = map(int, stdin.readline().split())
+gold = list(map(int, stdin.readline().split()))
+
+def knapsack(arr, weight, n):    
+    dp = [[False] * (n + 1) for _ in range(weight + 1)]
+    # print(*dp, sep = '\n')
+    dp[0][0] = True
+    for i in range(1, n + 1):
+        for w in range(weight + 1):
+            if arr[i-1] > w:
+                dp[w][i] = dp[w][i - 1]
+            else:
+                dp[w][i] = (dp[w][i - 1] or dp[w - arr[i - 1]][i - 1])
+    # print('ready dp')
+    # print(*dp, sep = '\n')
+    
+    for w in range(weight, -1, -1):
+        #for i in range(n, -1, -1):
+        if dp[w][n] == True:
+            return w
+    return 0
+print(knapsack(gold, w, n))
 
